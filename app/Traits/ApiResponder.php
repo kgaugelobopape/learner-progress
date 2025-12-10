1bf486d50e56121746922153e45ebb7e698fa05d
Kgaugelo Bopape <kgaugelo20@gmail.com>
2025-12-10 22:47:10 +0200

    Add api global responder

--- app/Traits/ApiResponder.php
+++ app/Traits/ApiResponder.php
@@ -2,12 +2,10 @@
 
 namespace App\Traits;
 
-use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
-use Illuminate\Support\Str;
 
-trait ApiResponser
+trait ApiResponder
 {
     /**
      * Build success response
@@ -18,20 +16,8 @@ trait ApiResponser
      * @param string $location
      * @return JsonResponse
      */
-    public function httpResponse(mixed $data, bool $success = true, int $code = Response::HTTP_OK, string $message = "message.operation_successful", string $location = "/welcome"): JsonResponse
+    public function httpResponse(mixed $data, bool $success = true, int $code = 200, string $message = "message.operation_successful", string $location = "/welcome"): JsonResponse
     {
         return response()->json(["data" => $data, "message" => $message, "success" => $success, "location" => $location], $code);
-    }
-
-    /**
-     * Build success response
-     * @param string|array $message
-     * @param int $code
-     * @param string $location
-     * @return JsonResponse
-     */
-    public function errorResponse($message, int $code, string $location = "/home"): JsonResponse
-    {
-        return response()->json(["error" => $message, "code" => $code, "location" => $location], $code);
     }
 }
