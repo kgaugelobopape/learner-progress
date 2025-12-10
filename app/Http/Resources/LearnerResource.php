d66215322056ef253d78121988cc0453862075f4
Kgaugelo Bopape <kgaugelo20@gmail.com>
2025-12-10 22:42:18 +0200

    Format response data

---
+++ app/Http/Resources/LearnerResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LearnerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->full_name,
+            "courses" => $this->whenLoaded("courses")->map(function ($course){
+                return [
+                    "id"       => $course->id,
+                    "name"     => $course->name,
+                    "progress" => $course->pivot->progress ?? 0
+                ];
+            }),
+            "lastname" => $this->lastname,
+            "firstname" => $this->firstname,
+            "average_progress" => $this->average_progress,
+        ];
+    }
+}
